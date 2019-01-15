542e3113fc1a5c3751b32629f29b791224d4a487
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-01-15 23:37:07 +0700

    :sparkle: Reading database values

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import mysql.connector
 from flask import Flask, jsonify
+from os import getenv
+from dotenv import load_dotenv
+load_dotenv()
+
 app = Flask(__name__)
+
+
+conn = mysql.connector.connect(
+    host=getenv("MYSQL_HOST"),
+    user=getenv("MYSQL_USERNAME"),
+    passwd=getenv("MYSQL_PASSWORD"),
+    database=getenv("MYSQL_DB")
+)
+
+cursor = conn.cursor()
 
 
 @app.route("/")
@@ -9,8 +24,16 @@ def hello():
 
 @app.route("/latest")
 def return_latest_dust():
+    sql = '''
+    SELECT *
+    FROM logs
+    WHERE timestamp = (SELECT MAX(timestamp) FROM logs)
+    '''
+    cursor.execute(sql)
+    latest_data = cursor.fetchone()
     return jsonify({
-        "PM10": 10,
-        "PM2.5": 10,
-        "PM1": 10
+        "timestamp": latest_data[0],
+        "PM10": latest_data[1],
+        "PM2.5": latest_data[2],
+        "PM1": latest_data[3]
     })
