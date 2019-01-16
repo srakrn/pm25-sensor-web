4fd1baaf7233d86dda244bc8468328f4ffe39b56
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-01-16 09:23:45 +0700

    :sparkle: API for updating values

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ load_dotenv()
 app = Flask(__name__)
 
 
-conn = mysql.connector.connect(
-    host=getenv("MYSQL_HOST"),
-    user=getenv("MYSQL_USERNAME"),
-    passwd=getenv("MYSQL_PASSWORD"),
-    database=getenv("MYSQL_DB")
-)
+@app.before_request
+def generate_conn():
+    global conn, cursor
+    conn = mysql.connector.connect(
+        host=getenv("MYSQL_HOST"),
+        user=getenv("MYSQL_USERNAME"),
+        passwd=getenv("MYSQL_PASSWORD"),
+        database=getenv("MYSQL_DB")
+    )
 
-cursor = conn.cursor()
+    cursor = conn.cursor()
 
 
 @app.route("/")
@@ -27,7 +30,7 @@ def return_latest_dust():
     sql = '''
     SELECT *
     FROM logs
-    WHERE timestamp = (SELECT MAX(timestamp) FROM logs)
+    ORDER BY timestamp DESC
     '''
     cursor.execute(sql)
     latest_data = cursor.fetchone()
@@ -67,3 +70,28 @@ def return_amounted_dust():
             "PM1": latest_data[3]
         })
     return jsonify(res)
+
+
+@app.route("/insert")
+def insert():
+    secret = str(request.args.get('secret'))
+    if secret != getenv("SECRET"):
+        return jsonify({
+            "error": "invalid secret"
+        })
+    sql = "INSERT INTO logs (`pm10`, `pm2.5`, `pm1`) VALUES (%s, %s, %s)"
+    try:
+        values = [int(i) for i in request.args.get('values').split(",")]
+    except AttributeError:
+        return jsonify({
+            "error": "invalid values"
+        })
+    if len(values) != 3:
+        return jsonify({
+            "error": "invalid values length"
+        })
+    cursor.execute(sql, values)
+    conn.commit()
+    return jsonify({
+        "status": "done"
+    })
