322bc4d1950b888bb42b9fc8ef5fc2eb7d3b1360
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-01-17 17:25:00 +0700

    Auto updating every 15 minutes

--- cpe_pm25/static/main.js
+++ cpe_pm25/static/main.js
@@ -1,3 +1,15 @@
+function updateValues() {
+  $.getJSON("/api/latest", function(data) {
+    console.log(data);
+    $("#pm25").html(data["PM2.5"]);
+    $("#pm10").html(data["PM10"]);
+    $("#pm1").html(data["PM1"]);
+    $("#pm1").html(data["PM1"]);
+    $("#timestamp").html(data["timestamp"]);
+  });
+}
+
 $(".carousel").carousel({
-    "interval": 2000;
+  interval: 3000
 });
+setInterval(updateValues, 15 * 60 * 1000);
