53c3487c2833d02310f37cd2ce7dd56e23d25507
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-01-19 15:17:24 +0700

    Using tabs instead of carousal

--- cpe_pm25/static/main.js
+++ cpe_pm25/static/main.js
@@ -1,10 +1,9 @@
 function updateValues() {
   $.getJSON("api/latest", function(data) {
     console.log(data);
-    $("#pm25").html(data["PM2.5"]);
-    $("#pm10").html(data["PM10"]);
-    $("#pm1").html(data["PM1"]);
-    $("#pm1").html(data["PM1"]);
+    $("#pm25-valie").html(data["PM2.5"]);
+    $("#pm10-value").html(data["PM10"]);
+    $("#pm1-value").html(data["PM1"]);
     var timestamp = moment(data["timestamp"]);
     $("#timestamp").html(timestamp.format("DD/MM/YYYY HH:mm"));
   });
@@ -41,6 +40,8 @@ function updateChart() {
         ]
       },
       options: {
+        responsive: true,
+        maintainAspectRatio: false,
         scales: {
           yAxes: [
             {
